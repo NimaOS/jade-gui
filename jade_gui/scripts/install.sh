9ee7b137886bf4694a67cec96cc5c3de3d5adeef
Nima <smartnimaco@gmail.com>
2023-12-08 15:12:03 +0330

    Make Jade-GUIs install.sh work with nimaos-inst

--- jade_gui/scripts/install.sh
+++ jade_gui/scripts/install.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
+echo "Thanks for choosing NimaOS, Installing..."
 echo "Running reflector to sort for fastest mirrors" | tee -a /tmp/jade-gui-output.txt
 pkexec reflector --latest 5 --sort rate --save /etc/pacman.d/mirrorlist | tee -a /tmp/jade-gui-output.txt
-pkexec crystal-jade config ~/.config/jade.json | tee -a /tmp/jade-gui-output.txt
+pkexec nimaos-inst config ~/.config/jade.json | tee -a /tmp/jade-gui-output.txt
